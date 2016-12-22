3f077a09d10bd4f2884b925ef7b043f52fe2d9c7
W3ndige <kariqem@gmail.com>
2016-12-22 21:47:32 +0100

    Small mess

--- js/main.js
+++ js/main.js
@@ -1,46 +1,52 @@
 $(function(){
+    /* Typed function on terminal */
     $(".element").typed({
       strings: [
-      'JavaScript <br> function addition(a, b) {<br><span style="margin-left: 2em;"> return a + b;</span><br>}',
-     'PHP <br> function addition($a, $b) {<br><span style="margin-left: 2em;"> return $a + $b;</span><br>}',
-     'Python <br> def addition(a, b):<br><span style="margin-left: 2em;"> return a + b</span><br>',
-     'Java <br> public int addition(int a, int b) {<br><span style="margin-left: 2em;"> return a + b;</span><br>}',
-     'C<br> int addition(int a, int b) {<br><span style="margin-left: 2em;"> return a + b;</span><br>}',
-     'C++ <br> int addition(int a, int b) {<br><span style="margin-left: 2em;"> return a + b;</span><br>}'
+      '<span class="l">JavaScript</span> <br> function <span class="nf">addition</span>(<span class="nv">a</span>, <span class="nv">b</span>) {<br><span style="margin-left: 2em;"> <span class="k">return</span> <span class="nv">a</span> + <span class="nv">b</span>;</span><br>}',
+     '<span class="l">PHP</span> <br> function <span class="nf">addition</span>(<span class="nv">$a</span>, <span class="nv">$b</span>) {<br><span style="margin-left: 2em;"> <span class="k">return</span> <span class="nv">$a</span> + <span class="nv">$b</span>;</span><br>}',
+     '<span class="l">Python</span> <br> def <span class="nf">addition</span>(a, b):<br><span style="margin-left: 2em;"> <span class="k">return</span> a + b</span><br>',
+     '<span class="l">Java</span> <br> public <span class="kt">int</span> <span class="nf">addition</span>(<span class="kt">int</span> <span class="nv">a</span>, <span class="kt">int</span> <span class="nv">b</span>) {<br><span style="margin-left: 2em;"> <span class="k">return</span> <span class="nv">a</span> + <span class="nv">b</span>;</span><br>}',
+     '<span class="l">C</span><br> <span class="kt">int</span> <span class="nf">addition</span>(<span class="kt">int</span> <span class="nv">a</span>, <span class="kt">int</span> <span class="nv">b</span>) {<br><span style="margin-left: 2em;"> <span class="k">return</span> <span class="nv">a</span> + <span class="nv">b</span>;</span><br>}',
+     '<span class="l">C++</span> <br> <span class="kt">int</span> <span class="nf">addition</span>(<span class="kt">int</span> <span class="nv">a</span>, <span class="kt">int</span> <span class="nv">b</span>) {<br><span style="margin-left: 2em;"> <span class="k">return</span> <span class="nv">a</span> + <span class="nv">b</span>;</span><br>}'
       ],
       typeSpeed: 100,
       loop:true,
     });
+
+    /* Scroll to element effect */
+
+    var scrollToElement = function(el, ms){
+      var speed = (ms) ? ms : 600;
+      $('html,body').animate({
+          scrollTop: $(el).offset().top
+      }, speed);
+    }
+
+    $(".intro-btn").click(function() {
+    scrollToElement('#intro', 600);
+    });
+
+    $(".how-works-btn").click(function() {
+    scrollToElement('#how-works', 600);
+    });
+
+    $(".about-programming-btn").click(function() {
+    scrollToElement('#about-programming', 600);
+    });
+
+    $(".about-project-btn").click(function() {
+    scrollToElement('#about-project', 600);
+    });
+
+    /* Highlight section */
+
+    $('body').scrollspy({
+      target: '.navbar-fixed-top',
+      offset: 51
+    });
+
+    /* Laptop Responsivness Sets width to the 1.5 height of the laptop*/
+
+    var cw = $('.laptop').height()*1.5;
+    $('.laptop').css({'max-width':cw+'px'});
 });
-
-var scrollToElement = function(el, ms){
-  var speed = (ms) ? ms : 600;
-  $('html,body').animate({
-      scrollTop: $(el).offset().top
-  }, speed);
-}
-
-$(".intro-btn").click(function() {
-scrollToElement('#intro', 600);
-});
-
-$(".howWorks-btn").click(function() {
-scrollToElement('#howWorks', 600);
-});
-
-$(".about-programming-btn").click(function() {
-scrollToElement('#about-programming', 600);
-});
-
-$(".about-project-btn").click(function() {
-scrollToElement('#about-project', 600);
-});
-
-$('body').scrollspy({
-  target: '.navbar-fixed-top',
-  offset: 51
-});
-
-
-var cw = $('.laptop').height()*1.5;
-$('.laptop').css({'max-width':cw+'px'});
