dd60789b5278bb505cba901f90ca52ae7d410a7d
w3ndige <W3ndige>
2017-01-08 16:22:25 +0100

    New header section forn languages

--- js/language.js
+++ js/language.js
@@ -68,4 +68,20 @@ $(function(){
       $('.summary-language').slideUp();
     }
   });
+
+  document.querySelector(".summary-lang-menu").addEventListener('click', function() {
+    var chart = window.chart = new EasyPieChart(document.querySelector('.chart'), {
+      easing: 'easeOutElastic',
+      delay: 3000,
+      barColor: '#69c',
+      trackColor: '#ace',
+      scaleColor: false,
+      lineWidth: 20,
+      trackWidth: 16,
+      lineCap: 'butt',
+      onStep: function(from, to, percent) {
+        this.el.children[0].innerHTML = Math.round(percent);
+      }
+    });
+  });
 })
