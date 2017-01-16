13320bfb6f862f1c96c020870bca7b36b61ca4ef
NoMoExcuses <ellucky4@gmail.com>
2017-01-16 21:51:58 +0100

    python content, fixed animation issues in start, faq sections

--- js/faq.js
+++ js/faq.js
@@ -1,18 +1,47 @@
-$('.question').click(function () {
-  if ($(this).next().is( ":hidden" ) ) {
-     $(this).next().slideDown();
-     $(this).find('.fa-arrow-down').addClass('rotation');
-     $(this).find('.fa-arrow-down').removeClass('rotationBack');
-  }
-  else {
-    $(this).next().slideUp();
+
+$(function(){
+  $('.question').click(function () {
+    if ($(this).next().is( ":hidden" ) ) {
+       $(this).next().slideDown();
+       $(this).find('.fa-arrow-down').addClass('rotation');
+       $(this).find('.fa-arrow-down').removeClass('rotationBack');
+    }
+    else {
+      $(this).next().slideUp();
+      $(this).find('.fa-arrow-down').addClass('rotationBack');
+      $(this).find('.fa-arrow-down').removeClass('rotation');
+    }
+  });
+
+  $('.answer').click(function () {
+    $(this).slideUp();
     $(this).find('.fa-arrow-down').addClass('rotationBack');
     $(this).find('.fa-arrow-down').removeClass('rotation');
-  }
-});
+  });
 
-$('.answer').click(function () {
-  $(this).slideUp();
-  $(this).find('.fa-arrow-down').addClass('rotationBack');
-  $(this).find('.fa-arrow-down').removeClass('rotation');
+/* Scroll to element effect */
+
+  var scrollToElement = function(el, ms){
+    var speed = (ms) ? ms : 600;
+    $('html,body').animate({
+        scrollTop: $(el).offset().top
+    }, speed);
+  }
+
+  $(".common-concerns-btn").click(function() {
+  scrollToElement('#common-concerns', 600);
+  console.log("ldgsoakfdas")
+  });
+
+  $(".how-to-lern-btn").click(function() {
+  scrollToElement('#how-to-lern', 600);
+  });
+
+  $(".program-languages-btn").click(function() {
+  scrollToElement('#program-languages', 600);
+  });
+
+  $(".about-project-btn").click(function() {
+  scrollToElement('#about-project', 600);
+  });
 });
--- js/start.js
+++ js/start.js
@@ -5,17 +5,17 @@ var clasa = '.que' + iter;
   $('.ans').click(function() {
     if($(clasa).hasClass( 'slide_in' )) $( clasa ).removeClass( 'slide_in' );
     if($(this).hasClass( 'slide_out' )) $( this ).removeClass( 'slide_out' );
-    if($(clasa).find(".answers").hasClass( 'slide_up' )) $(clasa).find(".answers").removeClass( 'slide_up' );
+    if($(clasa).find(".ans").hasClass( 'slide_up' )) $(clasa).find(".ans").removeClass( 'slide_up' );
 
     $( this ).addClass( 'slide_out' );
     $( clasa ).fadeOut( "slow" );
     if (iter==1) {
-      $('.back').css("display", "block");
+      $('.back').css("visibility", "visible");
     }
 
     iter++;
     clasa = '.que' + iter;
-    window.setTimeout(function(){$(clasa).fadeIn( "slow" );$(clasa).find(".answers").show().addClass('slide_up');}, 1000);
+    window.setTimeout(function(){$(clasa).fadeIn( "slow" );$(clasa).find(".ans").show().addClass('slide_up');}, 1000);
     if($(clasa).hasClass( 'slide_in' )) $( clasa ).removeClass( 'slide_in' );
 
   });
@@ -23,14 +23,14 @@ var clasa = '.que' + iter;
   $('.back').click(function() {
     //if($(clasa).hasClass( 'slide_in' )) $( clasa ).removeClass( 'slide_in' );
     if($('.ans').hasClass( 'slide_out' )) $( '.ans' ).removeClass( 'slide_out' );
-    if($(clasa).find(".answers").hasClass( 'slide_up' )) $(clasa).find(".answers").removeClass( 'slide_up' );
+    if($(clasa).find(".ans").hasClass( 'slide_up' )) $(clasa).find(".ans").removeClass( 'slide_up' );
 
     $( clasa ).fadeOut( "slow" );
     iter--;
     clasa = '.que' + iter;
     window.setTimeout(function(){$( clasa ).show().addClass( 'slide_in' );}, 1000);
     if(iter==1) {
-      $('.back').css("display", "none");
+      $('.back').css("visibility", "hidden");
     }
   });
 
