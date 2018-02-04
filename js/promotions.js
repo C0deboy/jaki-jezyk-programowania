de5d22bf2a2f6167ba41959b9940959f0aa357b6
C0deboy <ellucky4@gmail.com>
2018-02-04 21:17:04 +0100

    top books visual update, updated language data

--- js/promotions.js
+++ js/promotions.js
@@ -16,8 +16,7 @@ let promotionAdText = `Na ${promotionURL.hostname} jest <a href="${promotionURL}
 if (isPromotionActive()) {
   if (customMessage !== '') {
     promotionAdText = customMessage;
-  }
-  else {
+  } else {
     createPromotionAd();
   }
   createPromotionMessagePopup();
@@ -25,6 +24,7 @@ if (isPromotionActive()) {
 
 modifyBookButtons();
 styleBookButtons();
+addImagesForTopList();
 
 
 function isPromotionActive() {
@@ -108,4 +108,31 @@ function styleBookButtons() {
   document.querySelectorAll('.helion-ksiazkasm4 a').forEach((link) => {
     link.classList.add('main-btn');
   });
+}
+
+function addImagesForTopList() {
+  const topBooks = document.querySelector('.top-books');
+  let positionNum = 1;
+  if (topBooks) {
+    topBooks.querySelectorAll('a').forEach((a) => {
+      const bookId = a.href.replace('http://helion.pl/view/9102Q/', '').replace('.htm', '');
+      const book = document.createElement('div');
+      const link = document.createElement('a');
+      const img = document.createElement('img');
+      const position = document.createElement('span');
+      position.innerText = '#' + positionNum++;
+      position.classList.add('position');
+      img.src = `https://static01.helion.com.pl/global/okladki/181x236/${bookId}.jpg`
+      book.classList.add('book');
+      link.href = a.href;
+      book.appendChild(position);
+      link.appendChild(img);
+      book.appendChild(link);
+      topBooks.appendChild(book);
+      book.appendChild(a);
+      //a.parentElement.insertBefore(book, a);
+    });
+    topBooks.removeChild(topBooks.querySelector('ol'));
+    topBooks.style.textAlign = 'center';
+  }
 }
