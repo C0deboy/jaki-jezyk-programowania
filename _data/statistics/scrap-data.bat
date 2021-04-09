ac4189120d7cf63611e8b3164e4fd0d78852c9cb
C0deboy <ellucky4@gmail.com>
2021-04-09 16:37:25 +0200

    updated stats

--- _data/statistics/scrap-data.bat
+++ _data/statistics/scrap-data.bat
@@ -2,5 +2,5 @@
 :: java -DgithubIgnoreProjects="CS-Notes" -DgithubOnlyLangs="Java" -jar stats-scraper.jar --github
 java -DgithubIgnoreProjects="CS-Notes" -jar stats-scraper.jar --github --tiobe --spectrum --stack --meetup --versions
 python E:\Projects\jjp-analysis\generate-stats.py
-::python E:\Projects\jjp-analysis\get-courses.py
+python E:\Projects\jjp-analysis\get-courses.py
 pause
