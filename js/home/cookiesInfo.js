d0e88ceaea979a251ddb693f76123a1495eb311f
C0deboy <ellucky4@gmail.com>
2022-04-10 18:05:15 +0200

    fix cookies info

--- js/home/cookiesInfo.js
+++ js/home/cookiesInfo.js
@@ -1,12 +1,13 @@
 const cookiesAcceptedCookieName = 'cookies-accepted';
 const cookiesInfoEl = document.querySelector('.cookies-info');
 
-cookiesInfoEl.querySelector('.btn-close').addEventListener('click', () => {
-  cookiesInfoEl.classList.remove('show');
-  localStorage.setItem(cookiesAcceptedCookieName, '1');
-});
+cookiesInfoEl.querySelector('.btn-close')
+  .addEventListener('click', () => {
+    cookiesInfoEl.classList.remove('show');
+    localStorage.setItem(cookiesAcceptedCookieName, '1');
+  });
 
 if (localStorage.getItem(cookiesAcceptedCookieName) !== '1') {
-  cookiesInfoEl.style.display = 'flex';
+  cookiesInfoEl.classList.remove('d-none');
   cookiesInfoEl.classList.add('show');
 }
