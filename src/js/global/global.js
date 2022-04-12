c4268669508524a2a2f384c324f1af506c9e363a
C0deboy <ellucky4@gmail.com>
2022-04-13 00:57:18 +0200

    fix faq and optimize hljs

--- src/js/global/global.js
+++ src/js/global/global.js
@@ -3,11 +3,17 @@ import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
 
+// SCSS
 require('../../css/global/global.scss');
 
+// LIBS
 require('bootstrap');
+
+// POLYFILLS
 require('./forEachPolyfill');
 require('./parentElementAppendPolyfill');
+
+// CUSTOM
 require('./nav');
 require('./tooltips');
 require('../technologies-list');
--- src/js/home/home.js
+++ src/js/home/home.js
@@ -1,11 +1,25 @@
+// SCSS
 require('../../css/home/home.scss');
 
-const requireAll = require.context('./', true, /\.js$/);
-requireAll.keys()
-  .forEach((filename) => {
-    requireAll(filename);
-  });
-
-const hljs = require('highlight.js');
+// LIBS
+require('./typed');
+const hljs = require('highlight.js/lib/core');
+hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
+hljs.registerLanguage('cpp', require('highlight.js/lib/languages/cpp'));
+hljs.registerLanguage('php', require('highlight.js/lib/languages/php'));
+hljs.registerLanguage('java', require('highlight.js/lib/languages/java'));
+hljs.registerLanguage('ruby', require('highlight.js/lib/languages/ruby'));
+hljs.registerLanguage('python', require('highlight.js/lib/languages/python'));
+hljs.registerLanguage('c', require('highlight.js/lib/languages/c'));
+hljs.registerLanguage('swift', require('highlight.js/lib/languages/swift'));
+hljs.registerLanguage('csharp', require('highlight.js/lib/languages/csharp'));
+hljs.registerLanguage('kotlin', require('highlight.js/lib/languages/kotlin'));
+hljs.registerLanguage('scala', require('highlight.js/lib/languages/scala'));
 
 hljs.highlightAll();
+
+// CUSTOM
+require('./cookiesInfo');
+require('./laptop');
+require('./survey');
+require('./typing');
