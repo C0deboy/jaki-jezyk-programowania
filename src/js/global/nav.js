e9455c88789fc269d434dd8392e62c5d9369cab2
C0deboy <ellucky4@gmail.com>
2022-04-16 23:19:53 +0200

    fix navbar hiding on scroll

--- src/js/global/nav.js
+++ src/js/global/nav.js
@@ -1,6 +1,8 @@
-import { ScrollSpy } from 'bootstrap';
+import { Collapse, ScrollSpy } from 'bootstrap';
 
 const navbarCustom = document.querySelector('.navbar-custom');
+const siteNav = document.getElementById('site-nav');
+const navbarCollapse = new Collapse(navbarCustom.querySelector('.collapse'));
 
 let previousTop = 0;
 
@@ -15,6 +17,10 @@ function dynamicNavbar() {
     }
   } else {
     navbarCustom.classList.remove('is-visible');
+
+    if (siteNav.classList.contains('show')) {
+      navbarCollapse.hide();
+    }
 
     if (currentTop > headerHeight && !navbarCustom.classList.contains('is-fixed')) {
       navbarCustom.classList.add('is-fixed');
